92e2fd3e1560591b2fa1940280b50982c5bca1f9
indieisaconcept <me@indieisaconcept.com>
2014-07-06 10:06:56 +1000

    tests: formatting
    - tweaked test groupings for helpers
    
    Impact: low

--- test/boulevard_helpers_test.js
+++ test/boulevard_helpers_test.js
@@ -31,69 +31,87 @@ describe('boulevard', function() {
             var result = helper.should.be.a.Function;
         });
 
-        it ('should return a function if a single argument passed "helper helper"', function () {
-            var result = helper('rev include').should.be.a.Function;
-        });
+        describe('[ USE ]', function () {
 
-        it ('should throw an error if no name passed: helper()', function () {
-            (function () {
-                helper();
-            }).should.throw();
-        });
+            it ('should return a function if a single argument passed "helper helper"', function () {
+                var result = helper('rev include').should.be.a.Function;
+            });
 
-        it ('should throw an error if a handler is not a function: helper("name", [])', function () {
-            (function () {
-                helper('nohandler', []);
-            }).should.throw();
-        });
-
-        it ('should throw an error if a duplicate handler name is detected', function () {
-            (function () {
-                helper('rev', noop);
-            }).should.throw();
-        });
-
-        it ('should register a helper function', function () {
-            helper('mycustomhelper', noop);
-            var result = helper.registered.mycustomhelper.should.be.a.Function;
-        });
-
-        describe('[ REV ]', function () {
-
-            it('should modify filename to include a version', function () {
-
-                var handler    = helper('rev'),
-                    collection = JSON.parse(JSON.stringify(fixture)),
-                    result     = handler('/some/path', collection, {}, {
-                        version: '1.0.0'
-                    });
-
-                should(result[0]).eql('a_100.css');
-                should(result[1].href).eql('b_100.css');
-                should(result[2]).eql('a_100.js');
-                should(result[3].src).eql('b_100.js');
-
+            it ('should throw an error if no name passed: helper()', function () {
+                (function () {
+                    helper();
+                }).should.throw();
             });
 
         });
 
-        describe('[ INCLUDE ]', function () {
+        describe('[ REGISTER ]', function () {
 
-            it('should filter results based upon a set condition', function () {
+            it ('should throw an error if a helper name is not a function when registering: helper([], [])', function () {
+                (function () {
+                    helper([], noop);
+                }).should.throw();
+            });
 
-                var handler    = helper('include'),
-                    collection = JSON.parse(JSON.stringify(fixture)),
-                    result     = handler('/some/path', collection, {
-                        story: true
-                    });
+            it ('should throw an error if a handler is not a function when registering: helper("name", [])', function () {
+                (function () {
+                    helper('nohandler', []);
+                }).should.throw();
+            });
 
-                result = result.reduce(function (previous, current) {
-                    var item = current.src || current.href || current;
-                    previous.push(item);
-                    return previous;
-                }, []);
+            it ('should throw an error if a duplicate handler name is detected', function () {
+                (function () {
+                    helper('rev', noop);
+                }).should.throw();
+            });
 
-                should(result).eql(['a.css', 'a.js', 'b.js']);
+            it ('should register a helper function', function () {
+                helper('mycustomhelper', noop);
+                var result = helper.registered.mycustomhelper.should.be.a.Function;
+            });
+
+        });
+
+        describe('[ REGISTERED ]', function () {
+
+            describe('[ REV ]', function () {
+
+                it('should modify filename to include a version', function () {
+
+                    var handler    = helper('rev'),
+                        collection = JSON.parse(JSON.stringify(fixture)),
+                        result     = handler('/some/path', collection, {}, {
+                            version: '1.0.0'
+                        });
+
+                    should(result[0]).eql('a_100.css');
+                    should(result[1].href).eql('b_100.css');
+                    should(result[2]).eql('a_100.js');
+                    should(result[3].src).eql('b_100.js');
+
+                });
+
+            });
+
+            describe('[ INCLUDE ]', function () {
+
+                it('should filter results based upon a set condition', function () {
+
+                    var handler    = helper('include'),
+                        collection = JSON.parse(JSON.stringify(fixture)),
+                        result     = handler('/some/path', collection, {
+                            story: true
+                        });
+
+                    result = result.reduce(function (previous, current) {
+                        var item = current.src || current.href || current;
+                        previous.push(item);
+                        return previous;
+                    }, []);
+
+                    should(result).eql(['a.css', 'a.js', 'b.js']);
+
+                });
 
             });
 
